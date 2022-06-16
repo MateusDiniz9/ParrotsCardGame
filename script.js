813d8ca783b64fe5e10c757c35de7921212b643d
MateusDiniz <mateus.090998@gmail.com>
2022-06-16 12:58:40 -0300

    distribuição das cartas ok, clique das cartas ok, falta apenas ela voltar se não for igual a ultima

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let cartas = [];
+const cartas = ["./imagens/bobrossparrot.gif",
+"./imagens/explodyparrot.gif",
+"./imagens/fiestaparrot.gif",
+"./imagens/metalparrot.gif",
+"./imagens/revertitparrot.gif",
+"./imagens/tripletsparrot.gif",
+"./imagens/unicornparrot.gif",];
+const cartasEmJogo = [];
+const jogadas = [];
+let contador = 0;
+let last;
+function comparador() {
+    return Math.random() - 0.5;
+}
+
 
 function iniciarJogo(){
     let qntCartas = prompt("Com quantas cartas deseja jogar?");
@@ -7,11 +21,53 @@ function iniciarJogo(){
             qntCartas = prompt("Numero invalido!! Por favor digite um numero par entre 4 e 14!");
         }while(qntCartas%2 !== 0 || qntCartas > 14 || qntCartas < 4)
     }
-    colocarCartas(qntCartas);
+    for(let i = 0; i < (qntCartas/2); i++){
+        cartasEmJogo.push(cartas[i]);
+        cartasEmJogo.push(cartas[i]);
+    }
+    cartasEmJogo.sort(comparador);
+    colocarCartas(qntCartas, cartasEmJogo);
 }
 
-function colocarCartas(numero){
-    
+function colocarCartas(numero, array){
+    let local = document.querySelector(".jogo")
+    for(let i = 0; i < numero; i++){
+        local.innerHTML+= `        
+        <div>
+        <div class="cartas front" onclick="selecionar(this, ${[i]})">
+            <img src="./imagens/front.png">
+        </div>
+        <div class="cartas back">
+            <img src="${cartasEmJogo[i]}">
+        </div>
+        </div>  
+        `
+    }
+}
+
+function selecionar (elemento, indice) {
+    const pai = elemento.parentNode;
+    let back = pai.querySelector(".back");
+    back.classList.add("virada")
+    elemento.classList.add("virada");
+    contador++;
+    jogada(indice, elemento, back);
+}
+
+function jogada(indice, elemento, back){
+    if(contador%2 === 0){
+        back.classList.toggle("virada");
+        elemento.classList.toggle("virada");
+        console.log(contador);
+        console.log(elemento)
+        console.log(back)
+        if(cartasEmJogo[indice] !== cartasEmJogo[last]){
+            back.classList.toggle("virada");
+            elemento.classList.toggle("virada");
+        }
+    }else{
+        last = indice;
+    }
 }
 
 iniciarJogo();
